39c21627777fa4baeffce77c617f98d6b45eb959
JW Caterine <9396720+gam32bit@users.noreply.github.com>
2026-01-12 12:37:32 -0500

    Create init.py

---
+++ worklog/init.py
@@ -0,0 +1,20 @@
+"""
+Work Log - Simple CLI logging for projects and meetings.
+"""
+
+from . import config
+from . import templates
+from . import ui
+from . import io
+from . import parser
+from . import commands
+
+__all__ = ["config", "templates", "ui", "io", "parser", "commands"]
+```
+
+### .gitignore
+```
+__pycache__/
+*.pyc
+.DS_Store
+*:Zone.Identifier
